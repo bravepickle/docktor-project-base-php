58a5b79eab6673bf50a94539c81c1eec50e49f85
Victor Konovalov <vkon@ciklum.com>
2015-10-07 00:10:14 +0300

    backup before great changes

--- bin/provision.sh
+++ bin/provision.sh
@@ -27,3 +27,20 @@ docker-compose --version || (\
 
 echo Setup SupervisorD
 supervisord --version || (apt-get install supervisor -y)
+
+echo Setup NGINX
+nginx -v || (wget -q http://nginx.org/keys/nginx_signing.key -O- | apt-key add - && \
+    echo deb http://nginx.org/packages/ubuntu/ trusty nginx >> /etc/apt/sources.list && \
+    echo deb-src http://nginx.org/packages/ubuntu/ trusty nginx >> /etc/apt/sources.list && \
+    apt-get update && \
+    apt-get -y upgrade && \
+    apt-get -y install nginx && \
+    sed -i -e"s/worker_processes  1/worker_processes 5/" /etc/nginx/nginx.conf && \
+    sed -i -e"s/keepalive_timeout\s*65/keepalive_timeout 2/" /etc/nginx/nginx.conf && \
+    sed -i -e"s/keepalive_timeout 2/keepalive_timeout 2;\n\tclient_max_body_size 100m/" /etc/nginx/nginx.conf && \
+    sed -i "s/.*conf\.d\/\*\.conf;.*/&\n    include \/etc\/nginx\/sites-enabled\/\*;/" /etc/nginx/nginx.conf && \
+    rm -Rf /etc/nginx/conf.d/* && \
+    mkdir -p /etc/nginx/sites-available/ && \
+    mkdir -p /etc/nginx/sites-enabled/ && \
+    mkdir -p /etc/nginx/ssl/ && \
+    nginx -v)
